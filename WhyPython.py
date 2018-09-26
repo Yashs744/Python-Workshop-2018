5f5809a77a5fae49a19f2534330170af39fdc53c
Yash Sharma <13031534+Yashs744@users.noreply.github.com>
2018-09-26 18:21:15 +0530

    Why Python?

---
+++ WhyPython.py
@@ -0,0 +1,9 @@
+# Why Python
+
+# List of Subject
+CS = "Operating System", "CSA", "C++", "Java", "Networking", "Linear Programming"
+
+# for each subject in the CS do
+# 	  display each subject
+for each_subject in CS:
+	print (each_subject)
